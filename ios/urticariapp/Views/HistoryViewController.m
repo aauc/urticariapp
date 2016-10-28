56621a7431e030133b6c78abf29f8470266f353f
enrique <enrique@enriquerodicio.local>
2016-10-28 16:35:58 +0200

    Export as csv

--- ios/urticariapp/Views/HistoryViewController.m
+++ ios/urticariapp/Views/HistoryViewController.m
@@ -13,6 +13,8 @@
 #import "INRegister+CoreDataClass.h"
 #import "INCalendarConstants.h"
 
+#import "INDataManager.h"
+
 @interface HistoryViewController ()
 
 @property (strong, nonatomic) IBOutlet UIView *containerCalendarView;
@@ -35,7 +37,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    [self setTitle:@"Historial"];
+    [self setTitle:NSLocalizedString(@"Historial", nil)];
+    
+    UIBarButtonItem *settingsButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Exportar", nil) style:UIBarButtonItemStyleDone target:self action:@selector(exportAsCsv)];
+    self.navigationItem.rightBarButtonItem = settingsButton;
     
     // Month Calendar.
     self.monthControllerView = [[INMonthViewController alloc] init];
@@ -82,6 +87,60 @@
 -(UIImage *)imageFromLevel:(NSInteger)level {
     NSString *imageName = [NSString stringWithFormat:@"uasLevel%ld",(long)level];
     return [UIImage imageNamed:imageName];
+}
+
+- (void)exportAsCsv {
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory , NSUserDomainMask, YES);
+    NSString *documentsDir = [paths objectAtIndex:0];
+    NSString *filePath = [documentsDir stringByAppendingPathComponent:@"urticaria.csv"];
+    NSURL *fileUrl = [NSURL fileURLWithPath:filePath];
+    
+    NSString *temp = @"";
+    
+    NSFetchRequest *fetch = [[NSFetchRequest alloc] init];
+    [fetch setEntity:[NSEntityDescription entityForName:@"INRegister" inManagedObjectContext:[INDataManager sharedManager].managedObjectContext]];
+    [fetch setSortDescriptors: @[[NSSortDescriptor sortDescriptorWithKey:@"dateTimeInterval" ascending:NO]]];
+    NSArray *result = [[INDataManager sharedManager].managedObjectContext executeFetchRequest:fetch error:nil];
+    
+    temp = [temp stringByAppendingString:[NSString stringWithFormat:@"%@, %@, %@, %@, %@, %@, %@\n", NSLocalizedString(@"Fecha", nil), NSLocalizedString(@"Puntuación Ronchas", nil), NSLocalizedString(@"Puntuación Picor", nil), NSLocalizedString(@"UAS", nil), NSLocalizedString(@"Angioedema", nil), NSLocalizedString(@"Limitaciones", nil), NSLocalizedString(@"Notas", nil)]];
+    
+    for (INRegister *reg in result) {
+        temp = [temp stringByAppendingString: [NSString stringWithFormat:@"%@/%@/%@,",reg.dateCompDay, reg.dateCompMonth, reg.dateCompYear]];
+        temp = [temp stringByAppendingString: [NSString stringWithFormat:@"%@,", reg.wheals]];
+        temp = [temp stringByAppendingString: [NSString stringWithFormat:@"%@,", reg.itch]];
+        temp = [temp stringByAppendingString: [NSString stringWithFormat:@"%@,", [NSNumber numberWithFloat:([reg.wheals floatValue] + [reg.itch floatValue])]]];
+        
+        if (reg.angioedema.integerValue) {
+            temp = [temp stringByAppendingString: @"\""];
+            for (int i = 0; i < [INRegister angioOptions]; i++) {
+                if ((reg.angioedema.integerValue & (long long)pow(2,i)) == (long long)pow(2, i)) {
+                    temp = [temp stringByAppendingString: [NSString stringWithFormat:@"%@\n", [INRegister nameAngioOption:pow(2, i)]]];
+                }
+            }
+            temp = [temp stringByAppendingString: @"\","];
+        } else {
+            temp = [temp stringByAppendingString: @","];
+        }
+        
+        if (reg.limitations.integerValue) {
+            temp = [temp stringByAppendingString: @"\""];
+            for (int i = 0; i < [INRegister limitOptions]; i++) {
+                if ((reg.limitations.integerValue & (long long)pow(2,i)) == (long long)pow(2, i)) {
+                    temp = [temp stringByAppendingString: [NSString stringWithFormat:@"%@\n", [INRegister nameLimitationsOption:pow(2, i)]]];
+                }
+            }
+            temp = [temp stringByAppendingString: @"\","];
+        } else {
+            temp = [temp stringByAppendingString: @","];
+        }
+        
+        temp = [temp stringByAppendingString: [NSString stringWithFormat:@"%@\n", reg.note == nil ? @"" : reg.note]];
+    }
+    
+    [temp writeToURL:fileUrl atomically:YES encoding:NSUTF8StringEncoding error:nil];
+    
+    UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems: @[@"Urticaria", fileUrl] applicationActivities:nil];
+    [self presentViewController:activityViewController animated:YES completion:nil];
 }
 
 
